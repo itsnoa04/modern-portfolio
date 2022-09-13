428f86b4e1dab2a5042254b813f9bcf32d362d44
Devamsh Manoj <devamshmanoj04@gmail.com>
2022-09-14 01:04:10 +0530

    error: set todo to fix husky

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import TestButton from "../components/base/button/TestButton";
 
 const Home: NextPage = () => {
+  //TODO: fix husky hooks not running
   return <h1>Welcome to home!</h1>;
 };
 
