fc5389a04fcd33fbd31b440bfe77edf34e6d5b3c
Devamsh Manoj <devamshmanoj04@gmail.com>
2022-09-15 22:40:40 +0530

    seo: added meta tags + title + description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,25 @@ import Image from "next/image";
 import TestButton from "../components/base/button/TestButton";
 
 const Home: NextPage = () => {
-  //TODO: fix husky hooks not running
-  return <h1>Welcome to home!</h1>;
+  return (
+    <>
+      <Head>
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Devamsh Manoj - Full Stack Developer </title>
+        <meta
+          name="description"
+          content="I build modern, creative, responsive, mobile-friendly, scalable, and practical web and mobile experiences delivered with high quality, performance, and speed. I am a Full Stack Developer with a passion for building great products. I have experience building web applications using React, NextJS, NodeJS, Express, MongoDB, MySQL, TypeScript, JavaScript, HTML, CSS, Bootstrap, Material UI, Tailwind CSS, and more. I aim to bring revolution to the way we perceive web and mobile applications by building exquisite applications that are beautiful, functional, and easy to use."
+        />
+        <meta
+          name="keywords"
+          content="Devamsh Manoj, Full Stack Developer, Javascript Developer, React Developer, Node JS Developer"
+        />
+        <meta name="author" content="Devamsh Manoj" />
+        <meta name="robots" content="index, follow" />
+      </Head>
+    </>
+  );
 };
 
 export default Home;
