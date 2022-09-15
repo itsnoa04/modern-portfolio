52381c5297ca7fee2a77325a435c388f09047582
Devamsh Manoj <devamshmanoj04@gmail.com>
2022-09-15 22:41:56 +0530

    test: removed unwanted test script

--- cypress/e2e/navigation.cy.ts
+++ cypress/e2e/navigation.cy.ts
@@ -2,9 +2,7 @@ context("home", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000");
   });
-  it("should have a title", () => {
-    cy.get("h1").should("contain", "Welcome to home!");
-  });
+  it("", () => {});
 });
 
 export {};
